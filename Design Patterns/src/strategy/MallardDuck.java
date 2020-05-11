160838e483a607547aa259f66e40198ee90ed46b
alexesca <alexescamore@gmail.com>
2020-05-11 16:44:14 -0500

    Added strategy pattern

---
+++ Design Patterns/src/strategy/MallardDuck.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class MallardDuck extends Duck{
+	public MallardDuck() {
+		quackBehaviour  = new Quack();
+		flyBehaviour = new FlyWithWings();
+	}
+	
+	public void display() {
+		System.out.println("I am a real Mallard Duck");
+	}
+}
---
+++ Design Patterns/src/strategy/Quack.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class Quack implements QuackBehaviour {
+	
+	@Override
+	public void quack() {
+		System.out.println("Quack that really quacks.");
+	}
+}
